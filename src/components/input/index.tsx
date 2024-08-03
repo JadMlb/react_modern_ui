884f9260c4a56f0a3686f2358c64872096686b1e
Jad <jad_mlb@outlook.com>
2024-08-03 11:21:07 +0300

    updated index of inputs

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,3 +1,4 @@
 export {default as Button} from "./button";
 export {default as CheckBox} from "./checkbox";
 export {default as ComboBox} from "./combobox";
+export {default as Input} from "./input";
