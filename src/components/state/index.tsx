ae53b2a8af45a59618b5727275bd8f055a2f2863
Jad <jad_mlb@outlook.com>
2024-08-05 17:48:57 +0300

    created ProgressBar component to show progress

--- src/components/state/index.tsx
+++ src/components/state/index.tsx
@@ -1,3 +1,4 @@
 export {default as SkeletonLoader} from "./skeleton_loader";
 export {default as Tag} from "./tag";
 export {default as Toaster} from "./toaster";
+export {default as ProgressBar} from "./progress_bar";
---
+++ src/components/state/progress_bar.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+import { colour, radius, spacing } from "../../styles/styles";
+import { useDarkMode, useTheme } from "../../styles/theme";
+import ThemeType from "../../types/theme";
+
+const Bar = styled.div<{$percentage: number, $thin?: boolean}>
+`
+	height: 100%;
+	min-height: ${props => props.$thin ? 3 : 10}px;
+	width: ${props => props.$percentage}%;
+	
+	background-color: black;
+	border-radius: ${radius.normal};
+
+	transition: width 250ms ease-in-out;
+`;
+
+const BarContainer = styled.div<{$isDark: boolean, $theme: ThemeType}>
+`
+	position: relative;
+	width: 100%;
+	background-color: ${props => colour (props.$isDark ? "grayDark" : "grayLight", props.$theme)};
+	border-radius: ${radius.normal};
+
+	> ${Bar}
+	{
+		background-color: ${props => colour ("primary", props.$theme)};
+	}
+`;
+
+const Container = styled.div
+`
+	min-height: 10px;
+	width: 100%;
+
+	display: grid;
+	grid-template-columns: 1fr 30px;
+	align-items: center;
+	gap: ${spacing.small};
+`;
+
+type ProgressBarProps = {
+	percentage: number,
+	thin?: boolean,
+	showPercentage?: boolean
+};
+
+export default function ProgressBar ({percentage, showPercentage, thin}: ProgressBarProps)
+{
+	const {theme} = useTheme();
+	const isDark = useDarkMode();
+	
+	if (percentage < 0 || percentage > 100)
+		throw new Error ("The percentage of a ProgressBar must be between 0 and 100");
+
+	return (
+		<Container>
+			<BarContainer $isDark = {isDark} $theme = {theme}>
+				<Bar $percentage = {percentage} $thin = {thin}/>
+			</BarContainer>
+			{
+				!thin && showPercentage && (percentage + "%")
+			}
+		</Container>
+	);
+}
