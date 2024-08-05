9c26e394daecf6dfb37cd520efe5228aee401f5a
Jad <jad_mlb@outlook.com>
2024-08-05 18:39:39 +0300

    fixed visual errors with progress bar animation and thickness

--- src/components/state/progress_bar.tsx
+++ src/components/state/progress_bar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { colour, radius, spacing } from "../../styles/styles";
 import { useDarkMode, useTheme } from "../../styles/theme";
 import ThemeType from "../../types/theme";
@@ -13,7 +13,7 @@ const Bar = styled.div<{$percentage: number, $thin?: boolean}>
 	background-color: black;
 	border-radius: ${radius.normal};
 
-	transition: width 250ms ease-in-out;
+	transition: width 100ms;
 `;
 
 const BarContainer = styled.div<{$isDark: boolean, $theme: ThemeType}>
@@ -29,13 +29,13 @@ const BarContainer = styled.div<{$isDark: boolean, $theme: ThemeType}>
 	}
 `;
 
-const Container = styled.div
+const Container = styled.div<{$thin?: boolean}>
 `
 	min-height: 10px;
 	width: 100%;
 
 	display: grid;
-	grid-template-columns: 1fr 30px;
+	grid-template-columns: 1fr ${props => !props.$thin && css `30px`};
 	align-items: center;
 	gap: ${spacing.small};
 `;
@@ -52,10 +52,10 @@ export default function ProgressBar ({percentage, showPercentage, thin}: Progres
 	const isDark = useDarkMode();
 	
 	if (percentage < 0 || percentage > 100)
-		throw new Error ("The percentage of a ProgressBar must be between 0 and 100");
+		throw new Error (`The percentage of a ProgressBar must be between 0 and 100, received ${percentage}.`);
 
 	return (
-		<Container>
+		<Container $thin = {thin}>
 			<BarContainer $isDark = {isDark} $theme = {theme}>
 				<Bar $percentage = {percentage} $thin = {thin}/>
 			</BarContainer>
