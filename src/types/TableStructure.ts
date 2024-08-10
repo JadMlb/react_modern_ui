0616ed156666d4a572d1ad6f331dfc1b59702cdf
Jad <jad_mlb@outlook.com>
2024-08-10 17:51:41 +0300

    added types used for tables

---
+++ src/types/TableStructure.ts
@@ -0,0 +1,35 @@
+export type TableStructure = {
+	/**
+	 * The column struture of the table
+	 */
+	columns: {
+		[columnName: string]: {
+			/**
+			 * The name to be displayed for this column. If not provided, the key used to identify this column is used. 
+			 */
+			displayName?: string,
+			/**
+			 * The fields in the `data` property to be added to this column. Different fields are separated by new lines. Fields can be referenced using object accessors. e.g. to access the field "b" under "a", "a.b" can be passed.
+			 */
+			fields: {
+				[fieldName: string]: TableRowField
+			}
+			/**
+			 * Defines the proportion that this column will take relative to the table. **The total proportions must add up to 1**.
+			 */
+			proportion: number
+		}
+	},
+	/**
+	 * Lists the keys of the columns that have a sorting button
+	 */
+	sortingColumns?: string[]
+};
+
+export type TableRowField = {
+	type: "number" | "text" | "tag",
+	/**
+	 * Specifies whether this field is important to display its contents in bold
+	 */
+	isPrimary?: boolean
+}
