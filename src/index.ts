8c3be0ee9b46b0738c4dddb6c0b7285d80b3f408
Jad <jad_mlb@outlook.com>
2024-08-13 12:45:41 +0300

    removed named export for theme components & hooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./components/input";
 export * from "./components/structure";
 export * from "./components/state";
-export * as theme from "./styles/theme";
+export * from "./styles/theme";
