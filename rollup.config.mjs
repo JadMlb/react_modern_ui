58b31ffead9daa1a0dccb4899c636ce6f6fd63d1
Jad <jad_mlb@outlook.com>
2024-09-24 20:41:19 +0200

    re released beta 12 with new compiled application

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert {type: "json"};
+import babel from "@rollup/plugin-babel";
 
 export default [
 	{
