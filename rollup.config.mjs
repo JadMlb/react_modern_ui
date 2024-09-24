ed27596544558bed4a1201a8c9a5ad6863661545
Jad <jad_mlb@outlook.com>
2024-09-24 20:38:36 +0200

    test 3: changed rollup config and tsconfig to try rearrange bundling process

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,20 +9,28 @@ export default [
 		input: "src/index.ts",
 		output: [
 			{
-			file: packageJson.main,
-			format: "cjs",
-			sourcemap: true,
+				file: packageJson.main,
+				format: "cjs",
+				sourcemap: true,
 			},
 			{
-			file: packageJson.module,
-			format: "esm",
-			sourcemap: true,
+				file: packageJson.module,
+				format: "esm",
+				sourcemap: true,
 			},
 		],
 		plugins: [
 			resolve(),
 			commonjs(),
 			typescript({ tsconfig: "./tsconfig.json" }),
+			babel (
+				{
+					exclude: "node_modules/**",
+					extensions: [".js", ".ts", ".tsx"],
+					babelHelpers: "bundled",
+					plugins: ["babel-plugin-styled-components"]
+				}
+			)
 		],
 		external: ["react", "react-dom", "styled-components"]
 	},
