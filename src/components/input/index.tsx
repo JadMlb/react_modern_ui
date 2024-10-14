5019a0708b7a7bc816dd64b83017a65eff0e9eeb
Jad <jad_mlb@outlook.com>
2024-10-14 10:48:18 +0200

    replaced input in index

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,7 @@
 export {default as Button} from "./button";
 export {default as CheckBox} from "./checkbox";
 export {default as ComboBox} from "./combobox";
-export {default as Input} from "./input";
+// export {default as Input} from "./input";
 export {default as RadioButtonsGroup} from "./radiobuttons_group";
 export {default as DraggableList} from "./draggable_list";
+export {default as Input} from "./input-new";
