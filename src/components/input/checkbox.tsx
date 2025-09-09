4b4e1a2bde1934db5655b9088b9bfe7f216ab4b4
JadMlb <jad_mlb@outlook.com>
2025-09-09 21:13:00 +0200

    passed the name prop to checkbox to be used in event.target.name

--- src/components/input/checkbox.tsx
+++ src/components/input/checkbox.tsx
@@ -75,7 +75,7 @@ function DashComponent ()
 /**
  * Renders a Checkbox component with specified value, either in normal checked/unchecked, or in tri-value
  */
-export default function Checkbox ({value, className, style, label, labelStyle, intermediateStyle, checkedStyle, checkedComponent = <CheckComponent/>, intermediateComponent = <DashComponent/>, onChange}: CheckboxProps)
+export default function Checkbox ({name, value, className, style, label, labelStyle, intermediateStyle, checkedStyle, checkedComponent = <CheckComponent/>, intermediateComponent = <DashComponent/>, onChange}: CheckboxProps)
 {
 	const isDark = useDarkMode();
 	const colour = useThemeColours();
@@ -152,6 +152,7 @@ export default function Checkbox ({value, className, style, label, labelStyle, i
 			</CheckBoxBox>
 			<HiddenInput
 				type = "checkbox"
+				name = {name}
 				checked = {isReallyChecked}
 				onChange = {e => onChange?. (e, e.target.value)}
 			/>
