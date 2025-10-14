31a4b1993e7f99f0c779f38d484d3c6f2343ea1e
JadMlb <jad_mlb@outlook.com>
2025-10-14 11:55:40 +0200

    fixed some syntax in interface declaration

--- src/types/components/input/datetime/GenericDateTimeInputProps.ts
+++ src/types/components/input/datetime/GenericDateTimeInputProps.ts
@@ -2,8 +2,8 @@ import { BasicInputProps } from "../BasicInputProps";
 
 export default interface GenericDateTimeInputProps extends BasicInputProps
 {
-	value?: string | Date,
-	type: "date" | "datetime" | "time"
+	value?: string | Date;
+	type: "date" | "datetime" | "time";
 	onChange?: (e: React.ChangeEvent | null, value: string | Date) => void;
 	/**
 	 * Defines the range, bounds included, of the value in the input field. If bound is null, it's equivalent to an infinity/unset. Defaults to `[-infinity, infinity]`.
