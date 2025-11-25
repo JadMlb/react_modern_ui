a56394b0d8ac4d9361ab283b665937626dcc4b6f
JadMlb <jad_mlb@outlook.com>
2025-11-25 13:57:08 +0100

    exported input base

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,5 +4,6 @@ export {default as Combobox} from "./input_types/combobox";
 export {default as RadioButtonsGroup} from "./radiobuttons_group";
 export {default as List} from "./list";
 export {default as Input} from "./input-new";
+export {default as InputBase} from "./input_types/input_base";
 export {default as Switch} from "./switch";
 export {default as Slider} from "./slider";
