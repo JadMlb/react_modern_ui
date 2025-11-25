105cded6960309dfcd3d3f2f11cfa0df0c024a30
JadMlb <jad_mlb@outlook.com>
2025-11-25 18:04:03 +0100

    [FIX]: fixed type exports for deleted and new input props types

--- src/types/components/input/text/index.ts
+++ src/types/components/input/text/index.ts
@@ -1,6 +1,4 @@
 export * from "./BasicTextInputProps";
-export * from "./EmailInputProps";
-export * from "./PasswordInputProps";
-export * from "./TextInputProps";
 export * from "./SingleLineTextInputProps";
 export * from "./MultiLineTextInputProps";
+export * from "./NonTextTextualInputProps";
