a1e79f0de961875900477fea4dab55b682f3fda3
JadMlb <jad_mlb@outlook.com>
2026-01-27 21:49:51 +0100

    [FEATURE]: made progress bar use progress html tag

--- src/components/state/progress_bar/bar.tsx
+++ src/components/state/progress_bar/bar.tsx
@@ -1,18 +1,29 @@
+import { useMemo } from "react";
 import { StaticStyle } from "../../../types";
-import ProgressBarBackground from "./bar_bg";
-import ProgressBarValue from "./value";
+import BaseProps from "../../../types/components/BaseProps";
 
-interface ProgressBarBarProps
+interface ProgressBarBarProps extends BaseProps
 {
+	percentage: number;
 	backgroundStyle?: StaticStyle;
 	style?: StaticStyle;
 }
 
-export default function ProgressBarBar ({backgroundStyle, style}: ProgressBarBarProps)
+export default function ProgressBarBar ({percentage, backgroundStyle, style, ...aria}: ProgressBarBarProps)
 {
+	const css = useMemo (
+		() => ({
+			"-webkit-appearance": "none",
+			"-moz-appearance": "none",
+			appearance: "none",
+			...backgroundStyle,
+			"::-webkit-progress-bar": backgroundStyle,
+			"::-webkit-progress-value": style,
+			"::-moz-progress-bar": style
+		} satisfies StaticStyle),
+		[backgroundStyle, style]
+	);
 	return (
-		<ProgressBarBackground style = {backgroundStyle}>
-			<ProgressBarValue style = {style}/>
-		</ProgressBarBackground>
+		<progress css = {css} max = {100} value = {percentage} {...aria}/>
 	);
 }
